7f57cdfacae191d7206aab83faec58022ac66e0c
base0x10 <jespy@gwu.edu>
2020-06-16 22:09:48 -0400

    Initial 0xhaust rust wrappers and types
    Some testing but not complete.
    
    I have finished sim.h wrappers and insn.h wrappers
    
    I need to wrap discore/dis1 as well as asm.h parsing

---
+++ build.rs
@@ -0,0 +1,25 @@
+extern crate bindgen;
+extern crate cc;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    cc::Build::new()
+        .file("0xhaust/asm.c")
+        .file("0xhaust/pspace.c")
+        .file("0xhaust/sim.c")
+        .warnings(false)
+        .compile("0xhaust");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .no_copy("SimState_t")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,4 @@
+#include "0xhaust/types.h"
+#include "0xhaust/asm.h"
+#include "0xhaust/sim.h"
+#include "0xhaust/insn.h"
