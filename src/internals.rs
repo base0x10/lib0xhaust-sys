6d737ed8ad1ff50397caf553f72206fd10beba0f
Joseph Espy <jespy@josephespy.com>
2022-01-04 10:15:48 -0800

    Ignore Null Pointer warnings generated when running bindgen's tests

--- src/internals.rs
+++ src/internals.rs
@@ -2,4 +2,9 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 #![allow(dead_code)]
+
+// The bindgen generated tests have a known issue in the allignment tests
+// where they play around with null pointers.  This UB is only generated
+// during cargo test
+#![allow(deref_nullptr)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
